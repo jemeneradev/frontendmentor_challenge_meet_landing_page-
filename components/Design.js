054b850655b00b82293d2d8fdefa553c3c9fb897
Jesus E. Menera <j.menera209@gmail.com>
2021-07-18 13:20:16 -0700

    Banner styling complete.

--- components/Design.js
+++ components/Design.js
@@ -4,7 +4,9 @@ export default function Home() {
         <div style={{position:"absolute",top:0,left:0,opacity:.5}}>
 
             {/* <Image src="/images/design-mobile.svg" width="375" height="2215" /> */}
-            <Image src="/images/design-tablet.svg" width="768" height="2128"/>
+             {/* <Image src="/images/design-tablet.svg" width="768" height="2128"/> */}
+            
+            {/* <Image src="/images/design-desktop.svg" width="1440" height="1895"/> */}
         </div>
     )
 }
