dd6bf86b8a8ebc3598e484fc1da4fa5889717e90
Jesus E. Menera <j.menera209@gmail.com>
2021-07-19 22:15:03 -0700

    Benefits tablet styling complete.

--- components/Design.js
+++ components/Design.js
@@ -3,8 +3,8 @@ export default function Home() {
     return (
         <div style={{position:"absolute",top:0,left:"26px",opacity:.5}}>
 
-            <Image src="/images/design-mobile.svg" width="375" height="2215" />
-             {/* <Image src="/images/design-tablet.svg" width="768" height="2128"/> */}
+            {/* <Image src="/images/design-mobile.svg" width="375" height="2215" /> */}
+             <Image src="/images/design-tablet.svg" width="768" height="2128"/>
             
             {/* <Image src="/images/design-desktop.svg" width="1440" height="1895"/> */}
         </div>
